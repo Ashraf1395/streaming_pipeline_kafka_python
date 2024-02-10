3ae6464b6b02e6f74e6cec3e92f2186bbf2a65bb
Ashraf1395 <1395ashraf@gmail.com>
2024-02-10 17:17:27 +0530

    Update from remote

--- streaming_on_avro/config.py
+++ streaming_on_avro/config.py
@@ -1,7 +1,7 @@
 INPUT_DATA_PATH = '../resources/rides.csv'
 
-RIDE_KEY_SCHEMA_PATH = '../resources/schemas/taxi_ride_key.avsc'
-RIDE_VALUE_SCHEMA_PATH = '../resources/schemas/taxi_ride_value.avsc'
+RIDE_KEY_SCHEMA_PATH = '../data/schemas/taxi_rides_key.avsc'
+RIDE_VALUE_SCHEMA_PATH = '../data/schemas/taxi_rides_value.avsc'
 
 SCHEMA_REGISTRY_URL = 'http://localhost:8081'
 BOOTSTRAP_SERVERS = 'localhost:9092'
